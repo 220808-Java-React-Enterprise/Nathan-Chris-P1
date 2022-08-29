c06cb1a4fd96d76759978501da9d63ebfd5eb2b8
Koukaakiva <koukaakiva@gmail.com>
2022-08-29 14:49:30 -0400

    fix ddl weirdness?

--- src/main/resources/scripts/chris-dql.sql
+++ src/main/resources/scripts/chris-dql.sql
@@ -2,9 +2,3 @@
 
 /*Selct*/
 select * from "users";
-select * from "ledgers";
-select * from "shipyards";
-select * from "shipClasses";
-select * from "ships";
-select * from "componentTypes";
-select * from "components";
