ef5152d05671882f24f977add8c0f7c65fe33615
Nathan <tahgtahv@gmail.com>
2022-08-30 11:29:02 -0400

    Clean up Reimb DAO

--- src/main/java/com/revature/daos/ReimbursementDAO.java
+++ src/main/java/com/revature/daos/ReimbursementDAO.java
@@ -42,56 +42,90 @@ public class ReimbursementDAO implements DAO<Reimbursement> {
     @Override
     public void delete(String id) {throw new InvalidSQLException("Not yet implemented.");}
 
+    private Reimbursement getRow(ResultSet rs) throws SQLException {
+        return new Reimbursement(
+                UUID.fromString(rs.getString("reimb_id")),
+                rs.getBigDecimal("amount"),
+                rs.getTimestamp("submitted"),
+                rs.getTimestamp("resolved"),
+                rs.getString("description"),
+                rs.getBytes("receipt"),
+                UUID.fromString(rs.getString("payment_id")),
+                UUID.fromString(rs.getString("author_id")),
+                UUID.fromString(rs.getString("resolver_id")),
+                ReimbursementStatus.valueOf(rs.getString("status_id")),
+                ReimbursementType.valueOf(rs.getString("type_id"))
+        );
+    }
+    
     @Override
     public Reimbursement getByKey(String key) {
-        try (Connection connection = ConnectionFactory.getInstance().getConnection()) {
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM ers_reimbursements WHERE reimb_id = ?");
+        try (Connection connection = ConnectionFactory.getInstance().getConnection();
+            PreparedStatement ps = connection.prepareStatement("SELECT * FROM ers_reimbursements WHERE reimb_id = ?")) {
             ps.setString(1, key);
             ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                return new Reimbursement(
-                        UUID.fromString(rs.getString("reimb_id")),
-                        rs.getBigDecimal("amount"),
-                        rs.getTimestamp("submitted"),
-                        rs.getTimestamp("resolved"),
-                        rs.getString("description"),
-                        rs.getBytes("receipt"),
-                        UUID.fromString(rs.getString("payment_id")),
-                        UUID.fromString(rs.getString("author_id")),
-                        UUID.fromString(rs.getString("resolver_id")),
-                        ReimbursementStatus.valueOf(rs.getString("status_id")),
-                        ReimbursementType.valueOf(rs.getString("type_id"))
-                );
-            }
+            if (rs.next()) return getRow(rs);
+            else return null;
         } catch (SQLException e) {
             e.printStackTrace();
             throw new InvalidSQLException("An error occurred when tyring to read from the database.");
         }
-        return null;
     }
 
     @Override
     public List<Reimbursement> getAll() {
         List<Reimbursement> reimbursements = new ArrayList<>();
-        try (Connection con = ConnectionFactory.getInstance().getConnection()) {
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM ers_reimbursements");
+        try (Connection con = ConnectionFactory.getInstance().getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM ers_reimbursements")) {
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                reimbursements.add(
-                        new Reimbursement(
-                                UUID.fromString(rs.getString("reimb_id")),
-                                rs.getBigDecimal("amount"),
-                                rs.getTimestamp("submitted"),
-                                rs.getTimestamp("resolved"),
-                                rs.getString("description"),
-                                rs.getBytes("receipt"),
-                                UUID.fromString(rs.getString("payment_id")),
-                                UUID.fromString(rs.getString("author_id")),
-                                UUID.fromString(rs.getString("resolver_id")),
-                                ReimbursementStatus.valueOf(rs.getString("status_id")),
-                                ReimbursementType.valueOf(rs.getString("type_id"))
-                        )
-                );
+                reimbursements.add(getRow(rs));
+            }
+        } catch (SQLException e) {
+            throw new InvalidSQLException("An error occurred when tyring to read from the database.");
+        }
+        return reimbursements;
+    }
+
+    public List<Reimbursement> getByType(ReimbursementType type) {
+        List<Reimbursement> reimbursements = new ArrayList<>();
+        try (Connection con = ConnectionFactory.getInstance().getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM ers_reimbursements WHERE type_id = ?")) {
+            ps.setString(1, type.toString());
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                reimbursements.add(getRow(rs));
+            }
+        } catch (SQLException e) {
+            throw new InvalidSQLException("An error occurred when tyring to read from the database.");
+        }
+        return reimbursements;
+    }
+
+    public List<Reimbursement> getByStatus(ReimbursementStatus status) {
+        List<Reimbursement> reimbursements = new ArrayList<>();
+        try (Connection con = ConnectionFactory.getInstance().getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM ers_reimbursements WHERE status_id = ?")) {
+            ps.setString(1, status.toString());
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                reimbursements.add(getRow(rs));
+            }
+        } catch (SQLException e) {
+            throw new InvalidSQLException("An error occurred when tyring to read from the database.");
+        }
+        return reimbursements;
+    }
+
+    public List<Reimbursement> getByTypeAndStatus(ReimbursementType type, ReimbursementStatus status) {
+        List<Reimbursement> reimbursements = new ArrayList<>();
+        try (Connection con = ConnectionFactory.getInstance().getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM ers_reimbursements WHERE type_id = ? AND status_id = ?")) {
+            ps.setString(1, type.toString());
+            ps.setString(2, status.toString());
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                reimbursements.add(getRow(rs));
             }
         } catch (SQLException e) {
             throw new InvalidSQLException("An error occurred when tyring to read from the database.");
