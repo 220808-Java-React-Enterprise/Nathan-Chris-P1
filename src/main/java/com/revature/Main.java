faa6c54051cc40979a14b5164079da284021bcad
Koukaakiva <koukaakiva@gmail.com>
2022-09-03 15:55:07 -0400

    Deploy branch

--- src/main/java/com/revature/Main.java
+++
@@ -1,9 +0,0 @@
-package com.revature;
-
-import java.util.UUID;
-
-public class Main {
-    public static void main(String[] args) {
-        //System.out.println(UUID.randomUUID());
-    }
-}
--- src/main/resources/scripts/p1-dql.sql
+++
@@ -1,5 +0,0 @@
-/*Data Query Language*/
-
-/*Selct*/
-select * from ers_users;
-select * from ers_reimbursements;
